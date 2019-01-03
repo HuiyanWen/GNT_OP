f4b269106e74a7b75ed7272d7d533e73d5d88ddc
HuiyanWen <35653098+HuiyanWen@users.noreply.github.com>
2019-01-03 16:41:14 +0800

    Update gnt_to_tfrecord.py

--- gnt_to_tfrecord.py
+++ gnt_to_tfrecord.py
@@ -7,16 +7,15 @@ import tensorflow as tf
 import os
 dstpath = '.'
 count = 0
-path = 'D:/TensorFlow_project/tfrecord_read&write/'
+path = 'H:/苏统华的空间/94类字母样本for荟俨/'
 flag = 0
 image = []
 tag_code = []
 result = {}
 ###################求总长度，保存到image_list中，num_shards为tfrecord的数目######################################
 for z in range(2, 3):
-    global result
     ###############生成字典###################
-    f = open('lexicon94.txt', 'rb')
+    f = open(path+'lexicon94.txt', 'rb')
     line = f.readline()
     i = 0
     while line:
@@ -24,7 +23,7 @@ for z in range(2, 3):
         i += 1
         line = f.readline()
 
-    ff = str(z) + '.gnt'
+    ff = path + str(z) + '.gnt'
     f = open(ff, 'rb')
     ###########################################
     length = os.path.getsize(ff)
@@ -32,14 +31,13 @@ for z in range(2, 3):
     point = 0
 
     while point < length:
-        global count
         count += 1
+
         #print('f.read(4):', f.read(4))
         #f.seek(-4, 1)
         length_bytes = struct.unpack('<I', f.read(4))[0]
         #print('length_bytes:', length_bytes)
         point += 4
-        global tag_code
         tag_code.append(f.read(2))
         #print('tag_code: ', tag_code)
         point += 2
@@ -51,13 +49,25 @@ for z in range(2, 3):
         point += 2
         image.append(f.read(width * height))
         point += width * height
+        # if count == 6000000:
+        #     del tag_code[:]
+        #     del image[:]
+        #     #print(tag_code[0])
+        #     #print(image[0])
+        # if count <= 6000000:
+        #      #print('count continue : ', count)
+        #      continue
+        # if count >= 9000000:
+        #     break
     f.close()
-print('count = ', count)
+#print('count - 6000000 = ', count-6000000)
 #print(tag_code[0], image[0])
 #print(image[count-1])
-#print('图片数:', count)
-length_per_shard = 1000  # 每个记录文件的样本长度
+print('图片数:', count)
+length_per_shard = 10000  # 每个记录文件的样本长度
+#num_shards = int(np.ceil((count-6000000) / length_per_shard))
 num_shards = int(np.ceil(count / length_per_shard))
+print('numshards:', num_shards)
 #################################################################################################################
 
 for index in range(num_shards):
